6b68a5ca81eddcdac10f90385f2a16403adcb99f
Sugandha Gupta <44794688+sugandhagupta94uf@users.noreply.github.com>
2020-08-29 19:37:46 -0400

    Create Assignment2_4.java

---
+++ src/Assignment2_4.java
@@ -0,0 +1,29 @@
+// Part 4
+
+import java.util.Scanner;
+
+public class Four
+{
+    public static void main(String[] args)
+    {
+        int no;
+        Scanner sc=new Scanner(System.in);
+
+        while(true)
+        {
+            System.out.println("Enter a no: ");
+            no=sc.nextInt();
+            if(no>=0)
+            {
+                System.out.println("Good Going");
+                continue;
+            }
+            else
+            {
+                System.out.println("Its Over");
+                break;
+            }
+
+        }
+    }
+}
