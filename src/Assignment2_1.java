15ba21a1af56fa8403ccecf1e1d79452f32fc04d
Sugandha Gupta <44794688+sugandhagupta94uf@users.noreply.github.com>
2020-08-29 19:34:55 -0400

    Create Assignment2_1.java

---
+++ src/Assignment2_1.java
@@ -0,0 +1,21 @@
+// Part 1:
+
+public static void div(int x){
+	
+	if (x%3==0 && x%5 == 0){
+System.out.println(“Consultadd JAVA Training”);
+return;
+}
+if (x%3 == 0){
+System.out.println(“Consultadd”);
+return;
+}
+if (x%5 == 0){
+System.out.println(“JAVA Training”);
+return;
+}
+else{
+System.out.println(“N/A”);
+return;}
+
+}
