0228573568bcd528c3fd948b1eabd2900c054664
Sugandha Gupta <44794688+sugandhagupta94uf@users.noreply.github.com>
2020-08-29 19:38:33 -0400

    Create Assignment2_6.java

---
+++ src/Assignment2_6.java
@@ -0,0 +1,31 @@
+// Part 6
+
+import java.util.Scanner;
+
+public class NewOld
+{
+    public static void main(String[] args)
+    {
+        int no;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a no: ");
+        no=sc.nextInt();
+
+        if(no%2!=0)
+            System.out.println("NEW");
+        else
+        {
+            if(no>=2 && no<=5)
+                System.out.println("OLD");
+            else
+            {
+                if(no>=6 && no<=30)
+                    System.out.println("NEW");
+                else
+                    System.out.println("OLD");
+            }
+        }
+
+
+    }
+}
