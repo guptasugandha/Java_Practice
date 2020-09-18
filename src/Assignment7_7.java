8200836d97a3f8b43e677c0293ee43c1074ef776
Sugandha Gupta <44794688+guptasugandha@users.noreply.github.com>
2020-09-17 23:27:52 -0400

    Create Assignment7_7.java

---
+++ src/Assignment7_7.java
@@ -0,0 +1,19 @@
+import java.io.*; 
+import java.util.*; 
+class OccurenceOfCharInString { 
+    static void characterCount(String inputString) 
+    { 
+        HashMap<Character, Integer> charCountMap = new HashMap<Character, Integer>();  
+        char[] strArray = inputString.toCharArray(); 
+        for (char c : strArray) { 
+            if (charCountMap.containsKey(c)) { 
+                charCountMap.put(c, charCountMap.get(c) + 1); 
+            } 
+            else { 
+                charCountMap.put(c, 1); 
+            } 
+        } 
+        for (Map.Entry entry : charCountMap.entrySet()) { 
+            System.out.println(entry.getKey() + " " + entry.getValue()); 
+        } 
+    }
