3757b246f98f70938161d8983736fdb69e7676c6
Sugandha Gupta <44794688+guptasugandha@users.noreply.github.com>
2020-09-17 23:02:57 -0400

    Create Assignment7_4.java

---
+++ src/Assignment7_4.java
@@ -0,0 +1,11 @@
+import java.util.*; 
+  
+class Unique { 
+    boolean uniqueCharacters(String str) 
+    { 
+        for (int i = 0; i < str.length(); i++) 
+            for (int j = i + 1; j < str.length(); j++) 
+                if (str.charAt(i) == str.charAt(j)) 
+                    return false;  
+        return true; 
+    }
