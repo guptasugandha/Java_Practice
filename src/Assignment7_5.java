381df25fb32faec37d95612ea91dda4d0e4801b3
Sugandha Gupta <44794688+guptasugandha@users.noreply.github.com>
2020-09-17 23:21:51 -0400

    Create Assignment7_5.java

---
+++ src/Assignment7_5.java
@@ -0,0 +1,27 @@
+public class WordCount { 
+   
+    static final int OUT = 0; 
+    static final int IN = 1; 
+       
+    static int countWords(String str) 
+    { 
+        int state = OUT; 
+        int wc = 0;  // word count 
+        int i = 0; 
+          
+        while (i < str.length()) 
+        { 
+            if (str.charAt(i) == ' ' || str.charAt(i) == '\n' 
+                    || str.charAt(i) == '\t') 
+                state = OUT; 
+                  
+            else if (state == OUT) 
+            { 
+                state = IN; 
+                ++wc; 
+            } 
+       
+            ++i; 
+        } 
+        return wc; 
+    }
